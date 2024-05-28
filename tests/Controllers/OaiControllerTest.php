ce7667183ed7020bede546e89a817091334aefe7
Pack <ppack254@gmail.com>
2024-05-28 17:08:58 +1200

    add trailing slash and SiteConfig module

--- tests/Controllers/OaiControllerTest.php
+++ tests/Controllers/OaiControllerTest.php
@@ -24,7 +24,8 @@ class OaiControllerTest extends FunctionalTest
     /**
      * @dataProvider badVerbUrlProvider
      */
-    public function testBadVerbResponse(string $requestUrl): void
+    public function testBadVerbResponse
+    (string $requestUrl): void
     {
         $response = $this->get($requestUrl);
         // Strip the get params for assertions to follow
